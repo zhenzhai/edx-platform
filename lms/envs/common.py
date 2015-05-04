e4ee7148b90272864a4c3ad55b46134244dac422
muhammad-ammar <mammar@gmail.com>
2015-05-04 15:03:32 +0500

    Unit Bookmarks List View
    TNL-1958

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1175,6 +1175,7 @@ courseware_js = (
         for pth in ['courseware', 'histogram', 'navigation']
     ] +
     ['js/' + pth + '.js' for pth in ['ajax-error']] +
+    ['js/bookmarks/main.js'] +
     sorted(rooted_glob(PROJECT_ROOT / 'static', 'coffee/src/modules/**/*.js'))
 )
 
