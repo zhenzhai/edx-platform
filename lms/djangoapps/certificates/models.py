7ee3a97086947214dfba476bbf1da8c6b8b4f3fc
Saleem Latif <saleem_ee@hotmail.com>
2015-12-14 12:40:50 +0500

    display certificate generate history in reverse chronological order

--- lms/djangoapps/certificates/models.py
+++ lms/djangoapps/certificates/models.py
@@ -331,8 +331,7 @@ class CertificateGenerationHistory(TimeStampedModel):
 
         # If students is present in task_input then, certificate generation task was run to
         # generate certificates for white listed students otherwise it is for all students.
-        # Translators: This string represents task was executed for students having exceptions.
-        return _("For exceptions") if 'students' in task_input_json else _("All learners")
+        return "For exceptions" if 'students' in task_input_json else "All learners"
 
     class Meta(object):
         app_label = "certificates"
