3f9c75f55505ac1552dd34de616489075e6313e9
J. Cliff Dyer <cdyer@edx.org>
2016-01-08 09:57:52 -0500

    Add other exception types.

--- openedx/core/lib/api/exceptions.py
+++ openedx/core/lib/api/exceptions.py
@@ -6,7 +6,7 @@ converted to JSON, like other API responses.
 from rest_framework import exceptions
 
 
-class DictAPIException(exceptions.APIException):
+class _DictAPIException(exceptions.APIException):
     """
     Intermediate class to allow exceptions to pass dict detail values.  Use by
     subclassing this along with another subclass of `exceptions.APIException`.
@@ -15,11 +15,84 @@ class DictAPIException(exceptions.APIException):
         if isinstance(detail, dict):
             self.detail = detail
         else:
-            super(DictAPIException, self).__init__(detail)
+            super(_DictAPIException, self).__init__(detail)
 
 
-class AuthenticationFailed(exceptions.AuthenticationFailed, DictAPIException):
+class AuthenticationFailed(exceptions.AuthenticationFailed, _DictAPIException):
     """
     Override of DRF's AuthenticationFailed exception to allow dictionary responses.
+    """
+    pass
+
+
+class MethodNotAllowed(exceptions.MethodNotAllowed, _DictAPIException):
+    """
+    Override of DRF's MethodNotAllowed exception to allow dictionary responses.
+    """
+    def __init__(self, method, detail=None):
+        if isinstance(detail, dict):
+            self.detail = detail
+        else:
+            super(MethodNotAllowed, self).__init__(method, detail)
+
+
+class NotAcceptable(exceptions.NotAcceptable, _DictAPIException):
+    """
+    Override of DRF's NotAcceptable exception to allow dictionary responses.
+    """
+
+    def __init__(self, detail=None, available_renderers=None):
+        self.available_renderers = available_renderers
+        if isinstance(detail, dict):
+            self.detail = detail
+        else:
+            super(NotAcceptable, self).__init__(detail, available_renderers)
+
+
+class NotAuthenticated(exceptions.NotAuthenticated, _DictAPIException):
+    """
+    Override of DRF's NotAuthenticated exception to allow dictionary responses.
+    """
+    pass
+
+
+class NotFound(exceptions.NotFound, _DictAPIException):
+    """
+    Override of DRF's NotFound exception to allow dictionary responses.
+    """
+    pass
+
+
+class ParseError(exceptions.ParseError, _DictAPIException):
+    """
+    Override of DRF's ParseError exception to allow dictionary responses.
+    """
+    pass
+
+
+class PermissionDenied(exceptions.PermissionDenied, _DictAPIException):
+    """
+    Override of DRF's PermissionDenied exception to allow dictionary responses.
+    """
+    pass
+
+
+class Throttled(exceptions.Throttled, _DictAPIException):
+    """
+    Override of DRF's Throttled exception to allow dictionary responses.
+    """
+    pass
+
+
+class UnsupportedMediaType(exceptions.UnsupportedMediaType, _DictAPIException):
+    """
+    Override of DRF's UnsupportedMediaType exception to allow dictionary responses.
+    """
+    pass
+
+
+class ValidationError(exceptions.ValidationError, _DictAPIException):
+    """
+    Override of DRF's ValidationError exception to allow dictionary responses.
     """
     pass
