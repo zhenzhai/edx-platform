2ef2973d9ced2876e7b462b58a2de70891b6b47d
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-07-10 20:16:05 -0700

    fix evaluate

--- common/lib/sandbox-packages/cluster/evaluate.py
+++ common/lib/sandbox-packages/cluster/evaluate.py
@@ -1,6 +1,6 @@
 import cluster_functions
 def evaluate(p):
 	final_pairs = cluster_functions.find_matches(p)
-	if len(final_pairs) == 1 and final_pairs[0][0] == ['R']:
+	if len(final_pairs) == 1 and final_pairs[0][0] == 'R':
 		return True
     
