3cb7977f9b46d60e9764b594e0647effb30bacc0
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-07-10 17:21:21 -0700

    fix evaluate.py

--- common/lib/sandbox-packages/cluster/evaluate.py
+++ common/lib/sandbox-packages/cluster/evaluate.py
@@ -1,3 +1,4 @@
+import cluster_functions
 def evaluate(p):
 	final_pairs = cluster_functions.find_matches(p)
 	if len(final_pairs) == 1 and final_pairs[0][0] == ['R']:
--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.1.5",
+    version="0.1.6",
     packages=[
         "loncapa",
         "verifiers",
