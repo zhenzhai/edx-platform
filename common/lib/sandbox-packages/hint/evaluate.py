50a52d9f9bdabec91486f52b0baca2733cbc253b
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-08 13:58:21 -0700

    fixed import error

--- common/lib/sandbox-packages/hint/evaluate.py
+++ common/lib/sandbox-packages/hint/evaluate.py
@@ -1,13 +1,13 @@
 import cluster_functions
 def evaluate(ans, att):
-	p = make_params(ans, att)
+	p = cluster_functions.make_params(ans, att)
 	final_pairs = cluster_functions.find_matches(p)
 	if len(final_pairs) == 1 and final_pairs[0][0] == 'R':
 		return True
 	return False
 
 def evaluate_w_variables(ans, att, variable_values, test_all=False):
-	matches = show_matching_group_w_variables(ans, att, variable_values, test_all)
+	matches = cluster_functions.show_matching_group_w_variables(ans, att, variable_values, test_all)
 	if len(matches) == 1 and matches[0] == att:
 		return True
 	return False
--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.2.9",
+    version="0.2.11",
     packages=[
         "loncapa",
         "verifiers",
