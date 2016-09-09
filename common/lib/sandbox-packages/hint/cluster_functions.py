053c8aee63884e923fd76115b1fea13e00cd51ec
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-09 14:39:22 -0700

    updated hint functions for answer with variables

--- common/lib/sandbox-packages/hint/cluster_functions.py
+++ common/lib/sandbox-packages/hint/cluster_functions.py
@@ -183,18 +183,26 @@ def get_numerical_answer(eval_tree):
 
 
 def equal_len(variable_list):
+    """ Check whether all variables has the same size of values to test """
     size_of_list = len(variable_list[variable_list.keys()[0]])
     for key in variable_list:
         if len(variable_list[key]) != size_of_list:
             return False
     return True
 
+def extract_one_value_to_test(variable_values, index):
+    """ Extract one value to test for each of the variable """
+    for key in variable_values:
+                variable_list[key] = variable_values[key][index]
+
+def make_params_w_variables(ans_str, att_str, variable_values, index):
+    """ make param using test value at the passed in index """
+    v_list = extract_one_value_to_test(variable_values, index)
+    return make_params(ans_str, att_str, v_list)
 
 def find_matches_w_variables(ans_str, att_str, variable_values, test_all=False):
-    variable_list = {}
-    for key in variable_values:
-        variable_list[key] = variable_values[key][0]
-    params = make_params(ans_str, att_str, variable_list)
+    """ return a list of hits """
+    params = make_params_w_variables(ans_str, att_str, variable_values, 0)
     final_matches = find_matches(params)
 
     if test_all:
@@ -205,20 +213,16 @@ def find_matches_w_variables(ans_str, att_str, variable_values, test_all=False):
 
         size_of_value_list = len(variable_values[variable_values.keys()[0]])
         for i in xrange(size_of_value_list):
-            variable_list = {}
-            for key in variable_values:
-                variable_list[key] = variable_values[key][i]
-            params = make_params(ans_str, att_str, variable_list)
+            params = make_params_w_variables(ans_str, att_str, variable_values, i)
             match = find_matches(params)
             if match != final_matches:
                 return None
     return final_matches
 
 
-
-
-
 def show_matching_group_w_variables(ans_str, att_str, variable_values, test_all=False):
+    """ can test all values provided in variable_values
+    and return a list of hits """
     matches = find_matches_w_variables(ans_str, att_str, variable_values, test_all)
     parse_tree_att = webwork_parser.parse_webwork(att_str)
     matching_exps = []
--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.2.18",
+    version="0.2.19",
     packages=[
         "loncapa",
         "verifiers",
