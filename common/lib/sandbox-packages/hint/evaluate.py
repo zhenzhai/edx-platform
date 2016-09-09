2eb78ff95906e527c7beded51263725bee313d4e
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-08 17:02:39 -0700

    removed return False

--- common/lib/sandbox-packages/hint/evaluate.py
+++ common/lib/sandbox-packages/hint/evaluate.py
@@ -4,13 +4,11 @@ def evaluate(p):
 	final_pairs = cluster_functions.find_matches(p)
 	if len(final_pairs) == 1 and final_pairs[0][0] == 'R':
 		return True
-	return False
 
 def evaluate_w_variables(ans, att, variable_values, test_all=False):
 	matches = cluster_functions.show_matching_group_w_variables(ans, att, variable_values, test_all)
 	if len(matches) == 1 and matches[0] == att:
 		return True
-	return False
 
 
     
