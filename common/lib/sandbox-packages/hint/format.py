2e31a626c117272ca14c0224b526a93bcdc1cbec
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-14 11:09:23 -0700

    change space to tab

--- common/lib/sandbox-packages/hint/format.py
+++ common/lib/sandbox-packages/hint/format.py
@@ -4,6 +4,6 @@ def format_u_hint(hint_text):
 def format_c_hint(hint_text, hitn_id, hint_answer, i):
 	hint_answer_id = hint_id[i] + "answer"
 	hint = "<button onclick='show_hint({0})'>Show hint</button>".format(i)
-    hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_id[i], hint_text)
-    hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_answer_id, hint_answer)
-    return hint
+	hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_id[i], hint_text)
+	hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_answer_id, hint_answer)
+	return hint
