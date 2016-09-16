0a90cb890ef03774f6b3a276a75cc0289a0acd6b
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-16 10:39:40 -0700

    update javascript

--- common/static/js/adaptive_hint.js
+++ common/static/js/adaptive_hint.js
@@ -71,48 +71,54 @@ function show_hint_in_problem() {
       }
     }
 }
+
 function show_hint_in_hint() {
-    var hintInputId = arguments[0];
-    var proId = arguments[1];
-    var hintTextId = arguments[2];
+    var index = arguments[0];
+    var hintInputId = arguments[1];
+    hintInputId = hintInputId[index];
+    var proId = arguments[2];
+    proId = proId[index];
+    var hintTextId = arguments[3];
+    hintTextId = hintTextId[index];
+
     var hintAnswerId = hintTextId + "answer";
     var tolerance = 1+Math.exp(-6);
   
     var hintId = hintInputId.replace('input', 'problem');
       hintId = hintId.slice(0, -4);
     
-      var proInputBox = document.getElementById(proId);
-      proId = proId.replace('input', 'status');
-      var prohintId = proId.replace('input', 'inputtype');
-      var ele = document.getElementById(proId);
-      if (ele.classList.contains("incorrect")) {
-        if (proInputBox.value != "") {
-          if (document.getElementById(hintTextId)) {
-            var hintText = document.getElementById(hintTextId).innerHTML;
-            document.getElementById(hintId).style.display = "";
-            document.getElementById(hintId).getElementsByTagName('p')[0].innerHTML = hintText;
-            var hintStatusId = hintInputId.replace('input', 'status');
+    var proInputBox = document.getElementById(proId);
+    proId = proId.replace('input', 'status');
+    var prohintId = proId.replace('input', 'inputtype');
+    var ele = document.getElementById(proId);
+    if (ele.classList.contains("incorrect")) {
+      if (proInputBox.value != "") {
+        if (document.getElementById(hintTextId)) {
+          var hintText = document.getElementById(hintTextId).innerHTML;
+          document.getElementById(hintId).style.display = "";
+          document.getElementById(hintId).getElementsByTagName('p')[0].innerHTML = hintText;
+          var hintStatusId = hintInputId.replace('input', 'status');
 
-            var inputValue = document.getElementById(hintInputId).value;
-            inputValue = eval(inputValue);
-            var answerValue = document.getElementById(hintAnswerId).innerHTML;
-            answerValue = answerValue.substring(1);
-            answerValue = answerValue.slice(0,-1);
-            answerValue = eval(answerValue);
+          var inputValue = document.getElementById(hintInputId).value;
+          inputValue = eval(inputValue);
+          var answerValue = document.getElementById(hintAnswerId).innerHTML;
+          answerValue = answerValue.substring(1);
+          answerValue = answerValue.slice(0,-1);
+          answerValue = eval(answerValue);
 
-            if (answerValue == 0) {
-              if (inputValue == 0) {
-                document.getElementById(hintStatusId).className = "correct";
-              } else {
-                document.getElementById(hintStatusId).className = "incorrect";
-              }
+          if (answerValue == 0) {
+            if (inputValue == 0) {
+              document.getElementById(hintStatusId).className = "correct";
             } else {
-              var ratio = inputValue / answerValue;
-                if (ratio < tolerance && ratio > (1/tolerance)) {
-                    document.getElementById(hintStatusId).className = "correct";
-                }
+              document.getElementById(hintStatusId).className = "incorrect";
             }
+          } else {
+            var ratio = inputValue / answerValue;
+              if (ratio < tolerance && ratio > (1/tolerance)) {
+                  document.getElementById(hintStatusId).className = "correct";
+              }
           }
         }
       }
-  }
+    }
+}
