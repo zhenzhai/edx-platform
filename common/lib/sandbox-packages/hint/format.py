10b4b3804179349e84ce5cbcb3138c0092faf50c
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-16 15:06:05 -0700

    fix formater

--- common/lib/sandbox-packages/hint/format.py
+++ common/lib/sandbox-packages/hint/format.py
@@ -2,6 +2,8 @@ def format_u_hint(hint_text):
 	return "<font color='grey'>Hint: {0}</font>".format(hint_text)
 
 def format_c_hint(hint_text, hint_id, hint_answer, i):
+	if i >= len(hint_id):
+		return ""
 	hint_answer_id = hint_id[i] + "answer"
 	hint = "<button onclick='show_hint_in_problem({0})'>Show hint</button>".format(i)
 	hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_id[i], hint_text)
