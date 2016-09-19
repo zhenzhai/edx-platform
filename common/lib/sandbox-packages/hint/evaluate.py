de177c233c6eab64d9e1cdbe5208e5b22a028e5c
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-18 23:33:27 -0700

    updated evaluate and format python files

--- common/lib/sandbox-packages/hint/evaluate.py
+++ common/lib/sandbox-packages/hint/evaluate.py
@@ -8,8 +8,14 @@ def evaluate(ans, att):
 	p = cluster_functions.make_params(ans, att)
 	if p == {}:
 		return False
+	print p
+	att_value = cluster_functions.get_numerical_answer(p['att_tree'])
+	ans_value = cluster_functions.get_numerical_answer(p['ans_tree'])
 	final_pairs = cluster_functions.find_matches(p)
+
 	if len(final_pairs) == 1 and final_pairs[0][0] == 'R':
+		return True
+	elif att_value == ans_value:
 		return True
 	else:
 		return False
@@ -25,8 +31,3 @@ def evaluate_w_variables(ans, att, variable_values, test_all=False):
 		return True
 	else:
 		return False
-
-def test():
-	print evaluate('\[0\]', '0')
-
-test()
--- common/lib/sandbox-packages/hint/format.py
+++ common/lib/sandbox-packages/hint/format.py
@@ -5,7 +5,9 @@ def format_c_hint(hint_text, hint_id, hint_answer, i):
 	if i >= len(hint_id):
 		return ""
 	hint_answer_id = hint_id[i] + "answer"
-	hint = "<button onclick='show_hint_in_problem({0})'>Show hint</button>".format(i)
+	hint = "<div style='padding:10px 10px 10px 0px;'>"
+	hint += "<button onclick='show_hint_in_problem({0})'>Show hint</button>".format(i)
 	hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_id[i], hint_text)
 	hint += "<p id='{0}' style='display:none'> {1} </p>".format(hint_answer_id, hint_answer)
+	hint += "</div>"
 	return hint
