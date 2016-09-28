3c6af895efc0a0b09a88c7feca25001265db9b14
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-28 13:03:30 -0700

    manual import

--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.2.43",
+    version="0.2.44",
     packages=[
         "loncapa",
         "verifiers",
         "hint",
+        "hint.hint_class_helpers",
+        "hint.hint_class",
+        "hint.hint_class.first_Universal",
+        "hint.hint_class.last_Universal"
     ],
     py_modules=[
         "eia",
