874a313e507b118720e90c841577214ac56c2895
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-09-28 13:24:08 -0700

    manually import expr_parser and all the hint classes

--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,15 +2,24 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.2.44",
+    version="0.2.45",
     packages=[
         "loncapa",
         "verifiers",
         "hint",
         "hint.hint_class_helpers",
+        "hint.hint_class_helpers.expr_parser",
         "hint.hint_class",
         "hint.hint_class.first_Universal",
-        "hint.hint_class.last_Universal"
+        "hint.hint_class.last_Universal",
+        "hint.hint_class.Week2",
+        "hint.hint_class.Week3",
+        "hint.hint_class.Week4",
+        "hint.hint_class.Week5",
+        "hint.hint_class.Week6",
+        "hint.hint_class.Week7",
+        "hint.hint_class.Week8",
+        "hint.hint_class.Week9"
     ],
     py_modules=[
         "eia",
