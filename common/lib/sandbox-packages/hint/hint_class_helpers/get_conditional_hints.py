d6ce6e78a93a0efa1fd731f2d89bd74b148cb881
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-10-12 12:04:51 -0700

    recommit all changes in one commit

--- common/lib/sandbox-packages/hint/hint_class_helpers/get_conditional_hints.py
+++ common/lib/sandbox-packages/hint/hint_class_helpers/get_conditional_hints.py
@@ -1,41 +1,44 @@
 def get_conditional_hints(hint_text_id, i, params):
-    if i >= len(hint_id):
+    if i >= len(hint_text_id):
         #logger.error("index out of range in get_conditional_hints")
-        return ""
+        return "", ""
+    id_str = hint_text_id[i]
 
-    path = ""
-    # TODO: search from DB 
+    # id parsing
+    try:
+        week_pos, problem_pos, part_pos = \
+                id_str.find('week'), id_str.find('problem'), id_str.find('part')
+        week_id, problem_id, part_id = \
+                id_str[week_pos+4:problem_pos], id_str[problem_pos+7:part_pos], id_str[part_pos+4:]
+    except:
+        print "input hint_text_id incorrect."
+        return "", ""
+    
+    class_address = "hint.hint_class.Week{0}.Prob{1}_Part{2}.Prob{1}_Part{2}".format(week_id, problem_id, part_id)
+
 
     '''Read hint class'''
-    folder_name = path
-    hint_classes = []
-    for f in listdir(os.path.expanduser(folder_name)):
-        if isfile(join(os.path.expanduser(folder_name), f)) and \
-                f.endswith('.py') and f != '__init__.py' and f != 'template.py':
-            hint_classes.append(os.path.splitext(f)[0])
-
-    folder_name = path
-    package_name = folder_name.replace('/', '.')
-    for class_name in hint_classes:
-        class_address = "hint." + package_name + "." + class_name + "." + class_name
-        try:
-            ClassName = locate(class_address)
-        except:
-            print "ERROR: syntax error in HINT CLASS!!"
-            return
+    #class_address = "hint." + package_name + "." + class_name + "." + class_name
+    try:
+        ClassName = locate(class_address)
+    except:
+        print "ERROR: syntax error in HINT CLASS!!"
+        return "", ""
 
         try:
-            hint_instance = ClassName()
-        except TypeError:
-            print "ERROR: name of the HINT CLASS has to be the same as the name of the FILE !!"
-            return
+        hint_instance = ClassName()
+    except TypeError:
+        print "ERROR: name of the HINT CLASS has to be the same as the name of the FILE !!"
+        return "", ""
 
-        try:
-            hint, hint_ans = hint_instance.check_attempt(params)
-        except:
-            print "ERROR: syntax error in HINT CLASS!!"
-            return
+    try:
+        hint, hint_ans = hint_instance.check_attempt(params)
+    except:
+        print "ERROR: syntax error in HINT CLASS!!"
+        return
 
-        if hint:
-            return hint, hint_ans
+    if hint and hint_ans:
+        return hint, hint_ans
+
+    return "", ""
 
--- common/lib/sandbox-packages/setup.py
+++ common/lib/sandbox-packages/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sandbox-packages",
-    version="0.2.79",
+    version="0.2.80",
     packages=[
         "loncapa",
         "verifiers",
