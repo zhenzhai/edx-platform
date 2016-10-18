4af9924eabe51b8c1eaad7dd450065cdae1a0b5e
Zhen Zhai <zzhai@eng.ucsd.edu>
2016-10-17 23:43:36 -0700

    update flask script

--- hint_scripts/flask_script.py
+++ hint_scripts/flask_script.py
@@ -8,13 +8,21 @@ import traceback
 import logging.handlers
 import logging
 # logging settings
+formatter = logging.Formatter('%(asctime)s - %(name)s: %(levelname)s - %(message)s')
+
 log_path = '/home/zzhai/hint_log/show_hint.log'
 logger = logging.getLogger('show_hint')
 handler = logging.handlers.RotatingFileHandler(log_path, maxBytes = 262144, backupCount = 16)
-formatter = logging.Formatter('%(asctime)s - %(name)s: %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
+
+log_path2 = '/home/zzhai/hint_log/select_student.log'
+logger2 = logging.getLogger('select_student')
+handler2 = logging.handlers.RotatingFileHandler(log_path2, maxBytes = 262144, backupCount = 16)
+handler2.setFormatter(formatter)
+logger2.addHandler(handler2)
+logger2.setLevel(logging.INFO)
 
 
 db = MySQLdb.connect("localhost","root","","ucsd_cse103" )
@@ -48,7 +56,7 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/show_hint_button_clicked', methods=['POST'])
-def index():
+def hint_button():
     logger.info("captured post")
     student_username = request.form["student_name"]
     problem_info = request.form["problem_info"]
@@ -83,5 +91,59 @@ def index():
     
     logger.info("return success")
     return 'success'
+
+
+@app.route('/hint_permission', methods=['GET'])
+def select_student():
+    logger2.info("select_student log")
+    logger2.info("captured post")
+    username = request.form["username"]
+    problem_info = request.form["problem_info"]
+    timer_diff = request.form["timer_diff"]
+    hint_number = request.form["hint_number"]
+
+    try:
+        week_pos, problem_pos, part_pos = \
+             problem_info.find('week'), problem_info.find('problem'), problem_info.find('part')
+        week_id, problem_id, part_id = \
+                 problem_info[week_pos+4:problem_pos], problem_info[problem_pos+7:part_pos], problem_info[part_pos+4:]
+        week_number = "WEEK{0}".format(week_id)
+        logger2.info("record captured {0}".format(week_number))
+    except:
+        logger2.error("problem_info format wrong: {0}. Return False".format(problem_info))
+        return json.dumps({'status':'False','timer_diff':timer_diff, 'hint_number':hint_number})
+
+    logger2.info("grabbed username:{0}".format(username))
+    select_params = (username)
+    select_sql = """SELECT * FROM hint_assignment
+                    WHERE username = %s"""
+
+    try:
+        db_cursor.execute(select_sql,select_params)  
+        records = list(db_cursor.fetchall())
+        logger2.info("fetched list from database")
+        if(len(records) == 1):
+            student = records[0]
+            logger2.info("fetched student log permission of username:{0}.".format(username))
+            if student[week_number] == '1':
+                return json.dumps({'status':'True','timer_diff':timer_diff})
+            else:
+                return json.dumps({'status':'False','timer_diff':timer_diff})
+        elif(len(records) == 0):
+            logger2.info('No student is fetched with username: {0}. Return False'.format(username))
+            return json.dumps({'status':'False','timer_diff':timer_diff})
+        else:
+            logger2.info("Multiple students with the same username {0}. Return False".format(username))
+            return json.dumps({'status':'False','timer_diff':timer_diff})
+
+    except:
+        db.rollback()
+        logger2.error("Database has been rolled back because of an Exception below:")
+        logger2.error(traceback.format_exc())
+        logger2.error("Return False")
+        return json.dumps({'status':'False','timer_diff':timer_diff})
+    
+    logger2.error("Return False because of an error.")
+    return json.dumps({'status':'False','timer_diff':timer_diff})
 
 app.run(host='0.0.0.0')
